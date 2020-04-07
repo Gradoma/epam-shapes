07081c1f3ee16f229bd64fb7c5557d6ea0cf8c56
Gradoma <gradomski25@gmail.com>
2020-04-07 21:43:18 +0300

    overloaded method changeSphere added

--- src/by/epamtraining/shapes/repository/SphereRepository.java
+++ src/by/epamtraining/shapes/repository/SphereRepository.java
@@ -1,6 +1,8 @@
 package by.epamtraining.shapes.repository;
 
+import by.epamtraining.shapes.entity.Point;
 import by.epamtraining.shapes.entity.Sphere;
+import by.epamtraining.shapes.exception.IncorrectDataException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,6 +19,36 @@ public class SphereRepository {
 
     public boolean addSphere(Sphere sphere){
         return sphereList.add(sphere);
+    }
+
+    public boolean changeSphere(long id, double newRadius){
+        Specification idSpecification = new IdSpecification(id);
+        List<Sphere> sphereList = query(idSpecification);
+        if (sphereList.isEmpty()){
+            return false;
+        }
+        Sphere sphere = sphereList.get(0);
+        try{
+            sphere.setRadius(newRadius);
+        } catch (IncorrectDataException e){
+            return false;
+        }
+        return true;
+    }
+
+    public boolean changeSphere(long id, Point newCenterPoint){
+        Specification idSpecification = new IdSpecification(id);
+        List<Sphere> sphereList = query(idSpecification);
+        if (sphereList.isEmpty()){
+            return false;
+        }
+        Sphere sphere = sphereList.get(0);
+        try{
+            sphere.setCenterPoint(newCenterPoint);
+        } catch (IncorrectDataException e){
+            return false;
+        }
+        return true;
     }
 
     public boolean removeSphere(Sphere sphere){
--- src/by/epamtraining/shapes/runner.java
+++ src/by/epamtraining/shapes/runner.java
@@ -1,5 +1,6 @@
 package by.epamtraining.shapes;
 
+import by.epamtraining.shapes.entity.Point;
 import by.epamtraining.shapes.reader.SphereReader;
 import by.epamtraining.shapes.reader.impl.FileSphereReader;
 import by.epamtraining.shapes.entity.Shape;
@@ -8,30 +9,72 @@ import by.epamtraining.shapes.factory.ShapeFactory;
 import by.epamtraining.shapes.factory.SphereHolderList;
 import by.epamtraining.shapes.factory.impl.SphereFactory;
 import by.epamtraining.shapes.parser.Parser;
+import by.epamtraining.shapes.repository.RadiusSpecification;
+import by.epamtraining.shapes.repository.Specification;
+import by.epamtraining.shapes.repository.SphereRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class runner {
     public static void main(String[] args) throws Exception{
 
-        SphereReader dao = new FileSphereReader();
-        List<String> list = dao.getStrings("resource/file.txt");
-        
-        Parser parser = new Parser();
-        List<List<Double>> allSpheres = parser.parseValue(list);
-        ShapeFactory factory = new SphereFactory();
-        SphereHolderList sphereHolderList = SphereHolderList.getInstance();
+//        SphereReader dao = new FileSphereReader();
+//        List<String> list = dao.getStrings("resource/file.txt");
+//
+//        Parser parser = new Parser();
+//        List<List<Double>> allSpheres = parser.parseValue(list);
+//        ShapeFactory factory = new SphereFactory();
+//        SphereHolderList sphereHolderList = SphereHolderList.getInstance();
+//
+//        for (List<Double> valuesList: allSpheres){
+//            Shape sphere = factory.create(valuesList);
+//            sphereHolderList.addSphereToList((Sphere) sphere);
+//        }
+//        for (int i=0; i<sphereHolderList.getSphereList().size(); i++){
+//            Sphere s = sphereHolderList.getSphereList().get(i);
+//            System.out.print(s.getId() + " ");
+//            System.out.print(s.getCenterPoint() + " ");
+//            System.out.print(s.getRadius() + "\n");
+//        }
+        runner runner = new runner();
+        List<Double> testValue1 = new ArrayList<>();
+        testValue1.add(2.5);
+        testValue1.add(3.5);
+        testValue1.add(5.2);
+        testValue1.add(10.0);
+        SphereFactory factory = new SphereFactory();
+        Sphere sphere1 = (Sphere) factory.create(testValue1);
 
-        for (List<Double> valuesList: allSpheres){
-            Shape sphere = factory.create(valuesList);
-            sphereHolderList.addSphereToList((Sphere) sphere);
-        }
-        for (int i=0; i<sphereHolderList.getSphereList().size(); i++){
-            Sphere s = sphereHolderList.getSphereList().get(i);
-            System.out.print(s.getId() + " ");
-            System.out.print(s.getCenterPoint() + " ");
-            System.out.print(s.getRadius() + "\n");
-        }
+        List<Double> testValue2 = new ArrayList<>();
+        testValue2.add(2.5);
+        testValue2.add(3.5);
+        testValue2.add(5.2);
+        testValue2.add(15.4);
+        Sphere sphere2 = (Sphere) factory.create(testValue2);
 
+        SphereRepository repo = SphereRepository.getInstance();
+        repo.addSphere(sphere1);
+        repo.addSphere(sphere2);
+        runner.printList(repo.sphereList);
+
+        Specification testSpecification = new RadiusSpecification(12.0, 17.0);
+        List<Sphere> testSphereList = repo.query(testSpecification);
+        Sphere targetSphere = testSphereList.get(0);
+        System.out.println(targetSphere);           // target print
+
+        long targetId = 3;
+        repo.changeSphere(targetId, 255.2);
+
+        System.out.println("repo after changes: ");      //test print
+        runner.printList(repo.sphereList);
+    }
+
+    public void printList(List<Sphere> list){
+        for(Sphere s: list){
+            System.out.println("s Id = " + s.getId());
+            System.out.println("s center = " + s.getCenterPoint());
+            System.out.println("s radius = " + s.getRadius());
+        }
     }
 }
