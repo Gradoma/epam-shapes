aed272965884a4d0c7e810d4c9d2c232ed9745df
Gradoma <gradomski25@gmail.com>
2020-04-12 11:30:59 +0300

    logger added to SphereRepository

--- src/by/epamtraining/shapes/repository/SphereRepository.java
+++ src/by/epamtraining/shapes/repository/SphereRepository.java
@@ -23,20 +23,25 @@ public class SphereRepository {
     }
 
     public boolean addSphere(Sphere sphere){
-        logger.info("parameter: Sphere for adding: " + sphere);
+        logger.debug("parameter: Sphere for adding: " + sphere);
         return sphereList.add(sphere);
     }
 
     public boolean changeSphere(long id, double newRadius){
+        logger.info("called method to change radius of sphere");
+        logger.debug("parameters: id = " + id + ", radius = " + newRadius);
         Specification idSpecification = new IdSpecification(id);
         List<Sphere> sphereList = query(idSpecification);
         if (sphereList.isEmpty()){
+            logger.debug("No Sphere with specified Id");
             return false;
         }
         Sphere sphere = sphereList.get(0);
         try{
             sphere.setRadius(newRadius);
+            logger.info("Radius was changed successfully, new radius = " + sphere.getRadius());
         } catch (IncorrectDataException e){
+            logger.error("IncorrectDataException was thrown: " + e.getMessage());
             return false;
         }
         return true;
@@ -47,38 +52,51 @@ public class SphereRepository {
     }
 
     public boolean changeSphere(long id, Point newCenterPoint){
+        logger.info("called method to change center point of sphere");
+        logger.debug("parameters: id = " + id + ", center point = " + newCenterPoint);
         Specification idSpecification = new IdSpecification(id);
         List<Sphere> sphereList = query(idSpecification);
         if (sphereList.isEmpty()){
+            logger.debug("No Sphere with specified Id");
             return false;
         }
         Sphere sphere = sphereList.get(0);
         try{
             sphere.setCenterPoint(newCenterPoint);
+            logger.info("Center point was changed successfully, new center point = " + sphere.getCenterPoint());
         } catch (IncorrectDataException e){
+            logger.error("IncorrectDataException was thrown: " + e.getMessage());
             return false;
         }
         return true;
     }
 
     public boolean removeSphere(Sphere sphere){
-        logger.info("parameter: Sphere for removing: " + sphere);
-        return sphereList.remove(sphere);
+        logger.debug("parameter: Sphere for removing: " + sphere);
+        boolean result = sphereList.remove(sphere);
+        logger.debug("result of removing Sphere: " + result);
+        return result;
     }
 
     public List<Sphere> sort(Comparator comparator){
+        logger.debug("parameter: Comparator: " + comparator);
         List<Sphere> sortedList = new ArrayList<>(sphereList);
         sortedList.sort(comparator);
+        logger.debug("sorted List: " + sortedList);
         return sortedList;
     }
 
     public List<Sphere> query(Specification specification){
+        logger.debug("parameter: Specification: " + specification);
         List<Sphere> resultList = new ArrayList<>();
         for (Sphere current : sphereList){
+            logger.info("inside for: Sphere: " + current);
             if (specification.specify(current)){
                 resultList.add(current);
+                logger.info("sphere added to result List");
             }
         }
+        logger.debug("result List of query(): " + resultList);
         return resultList;
     }
 }
--- src/by/epamtraining/shapes/repository/specification_impl/IdSpecification.java
+++ src/by/epamtraining/shapes/repository/specification_impl/IdSpecification.java
@@ -2,16 +2,23 @@ package by.epamtraining.shapes.repository.specification_impl;
 
 import by.epamtraining.shapes.entity.Sphere;
 import by.epamtraining.shapes.repository.Specification;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class IdSpecification implements Specification {
     private long id;
+    private static Logger logger = LogManager.getLogger();
 
     public IdSpecification(long id){
         this.id = id;
+        logger.debug("created, id = " + this.id);
     }
 
     @Override
     public boolean specify(Sphere sphere){
-        return id == sphere.getId();
+        logger.debug("parameter: " + sphere);
+        boolean result = id == sphere.getId();
+        logger.debug("id == sphere.getId() : " + result);
+        return result;
     }
 }
